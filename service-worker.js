5bbdbf75eb42f70cf034f7dd46eda1c3d4aa3ba2
Orit Giat <73077516+oritgiat@users.noreply.github.com>
2026-09-03 17:05:01 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 // מטמון בסיסי כדי ש-MyWay ייחשב PWA תקין ויעבוד גם ללא רשת (קבצים מקומיים בלבד).
-const CACHE_NAME = "myway-cache-v21";
+const CACHE_NAME = "myway-cache-v22";
 
 // קבצים מקומיים בלבד. קבצים חיצוניים (Google/Leaflet) נטענים מהרשת.
 const APP_SHELL = [
